6afa88bcc7386044c86cfb522059f8f465259538
Tony Meyer <tony.meyer@canonical.com>
2026-03-13 16:29:02 +1300

    add tests. fix tox and pre-commit conflict.

--- src/charmhub_listing_review/update_issue.py
+++ src/charmhub_listing_review/update_issue.py
@@ -33,7 +33,7 @@ import json
 import pathlib
 import random
 import re
-import subprocess
+import subprocess  # noqa: S404
 import urllib.error
 import urllib.request
 from typing import TypedDict, cast
